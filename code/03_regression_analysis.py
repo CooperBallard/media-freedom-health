fe1a3e0add30f334d90b5b1e4c349748a4404ea1
CooperBallard <cooper.ballard.28@dartmouth.edu>
2026-06-04 15:59:21 -0400

    Update 03_regression_analysis.py

--- code/03_regression_analysis.py
+++ code/03_regression_analysis.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import statsmodels.formula.api as smf
  
-#full path to your merged_panel.csv (it lives in public_data, not data)
+#full path to your merged_panel.csv
 PANEL = "/Users/cooperballard/Desktop/QSS20-S26/public_data/merged_panel.csv"
  
 #load merged panel from the merging script (script 01)
